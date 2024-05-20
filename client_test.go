c1593cbc688f8e4eb2bd36eb3ba8b6f41c50b055
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-05-20 09:51:08 +0200

    initial commit

---
+++ client_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/sdk-fabric/starwars-go/sdk"
+	"github.com/apioo/sdkgen-go"
+	"testing"
+)
+
+func TestClient(t *testing.T) {
+
+	client, err := sdk.Build("my_token")
+	if err != nil {
+		t.Error(err)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sdk-fabric/notion-go
+
+go 1.18
+
+require github.com/apioo/sdkgen-go v1.0.3
